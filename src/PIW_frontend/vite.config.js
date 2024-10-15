a12d874273e7370dfeac07a2ebba13b041ddb888
Vickey-m-victor <bscs144j2022@students.tum.ac.ke>
2024-10-16 02:04:40 +0300

    made internet indentity possible

--- src/PIW_frontend/vite.config.js
+++ src/PIW_frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
-import environment from 'vite-plugin-environment';
 import vue from '@vitejs/plugin-vue';
+import environment from 'vite-plugin-environment';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../../.env' });
@@ -9,8 +9,12 @@ dotenv.config({ path: '../../.env' });
 export default defineConfig({
   build: {
     emptyOutDir: true,
+    rollupOptions: {
+      external: [],
+    },
   },
   optimizeDeps: {
+    include: ['@dfinity/auth-client'],
     esbuildOptions: {
       define: {
         global: 'globalThis',
@@ -27,13 +31,15 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    environment('all', { prefix: 'CANISTER_' }),
-    environment('all', { prefix: 'DFX_' }),
+    environment({
+      prefix: 'VITE_',
+    }),
   ],
   resolve: {
     alias: [
       { find: 'declarations', replacement: fileURLToPath(new URL('../declarations', import.meta.url)) },
       { find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
+      { find: 'dfx-generated', replacement: fileURLToPath(new URL('./src/declarations', import.meta.url)) },
     ]
   }
 });
