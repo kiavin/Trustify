54f843091738d0bd565245b3b9484e9ec498b74f
Vickey-m-victor <bscs144j2022@students.tum.ac.ke>
2024-10-19 17:20:01 +0300

    backend available

--- src/declarations/PIW_backend/PIW_backend.did.d.ts
+++ src/declarations/PIW_backend/PIW_backend.did.d.ts
@@ -2,6 +2,62 @@ import type { Principal } from '@dfinity/principal';
 import type { ActorMethod } from '@dfinity/agent';
 import type { IDL } from '@dfinity/candid';
 
-export interface _SERVICE { 'greet' : ActorMethod<[string], string> }
+export type DisputeDecision = { 'RefundBuyer' : null } |
+  { 'ReleaseFunds' : null };
+export interface EscrowAgreement {
+  'status' : EscrowStatus,
+  'terms' : string,
+  'agreed_at' : [] | [bigint],
+  'created_at' : bigint,
+  'seller' : Principal,
+  'release_method' : [] | [ReleaseMethod],
+  'funded_at' : [] | [bigint],
+  'buyer' : Principal,
+  'disputed_at' : [] | [bigint],
+  'escrow_id' : bigint,
+  'amount' : bigint,
+  'shipped_at' : [] | [bigint],
+  'resolved_at' : [] | [bigint],
+  'released_at' : [] | [bigint],
+}
+export type EscrowStatus = { 'Disputed' : null } |
+  { 'Refunded' : null } |
+  { 'GoodsShipped' : null } |
+  { 'Agreed' : null } |
+  { 'Funded' : null } |
+  { 'FundsReleased' : null } |
+  { 'Created' : null } |
+  { 'Resolved' : null };
+export type ReleaseMethod = { 'Icp' : null };
+export type Result = { 'Ok' : null } |
+  { 'Err' : string };
+export type Result_1 = { 'Ok' : EscrowAgreement } |
+  { 'Err' : string };
+export type Result_2 = { 'Ok' : [Principal, Principal] } |
+  { 'Err' : string };
+export type Result_3 = { 'Ok' : bigint } |
+  { 'Err' : string };
+export interface _SERVICE {
+  'agree_escrow' : ActorMethod<[bigint], Result>,
+  'cancel_escrow' : ActorMethod<[bigint], Result>,
+  'confirm_goods_received' : ActorMethod<[bigint, ReleaseMethod], Result>,
+  'confirm_goods_shipped' : ActorMethod<[bigint], Result>,
+  'confirm_icp_transfer' : ActorMethod<[bigint], Result>,
+  'fund_escrow' : ActorMethod<[bigint], Result>,
+  'get_escrow' : ActorMethod<[bigint], Result_1>,
+  'get_owner' : ActorMethod<[], [] | [Principal]>,
+  'get_participants' : ActorMethod<[bigint], Result_2>,
+  'initiate_escrow' : ActorMethod<
+    [Principal, Principal, bigint, string],
+    Result_3
+  >,
+  'list_my_escrows' : ActorMethod<[], Array<EscrowAgreement>>,
+  'open_dispute' : ActorMethod<[bigint], Result>,
+  'resolve_dispute' : ActorMethod<[bigint, DisputeDecision], Result>,
+  'set_admin' : ActorMethod<[Principal], Result>,
+  'set_off_chain_server' : ActorMethod<[Principal], Result>,
+  'set_owner' : ActorMethod<[Principal], undefined>,
+  'whoami' : ActorMethod<[], Principal>,
+}
 export declare const idlFactory: IDL.InterfaceFactory;
 export declare const init: (args: { IDL: typeof IDL }) => IDL.Type[];
--- src/declarations/PIW_backend/PIW_backend.did.js
+++ src/declarations/PIW_backend/PIW_backend.did.js
@@ -1,4 +1,68 @@
 export const idlFactory = ({ IDL }) => {
-  return IDL.Service({ 'greet' : IDL.Func([IDL.Text], [IDL.Text], ['query']) });
+  const Result = IDL.Variant({ 'Ok' : IDL.Null, 'Err' : IDL.Text });
+  const ReleaseMethod = IDL.Variant({ 'Icp' : IDL.Null });
+  const EscrowStatus = IDL.Variant({
+    'Disputed' : IDL.Null,
+    'Refunded' : IDL.Null,
+    'GoodsShipped' : IDL.Null,
+    'Agreed' : IDL.Null,
+    'Funded' : IDL.Null,
+    'FundsReleased' : IDL.Null,
+    'Created' : IDL.Null,
+    'Resolved' : IDL.Null,
+  });
+  const EscrowAgreement = IDL.Record({
+    'status' : EscrowStatus,
+    'terms' : IDL.Text,
+    'agreed_at' : IDL.Opt(IDL.Nat64),
+    'created_at' : IDL.Nat64,
+    'seller' : IDL.Principal,
+    'release_method' : IDL.Opt(ReleaseMethod),
+    'funded_at' : IDL.Opt(IDL.Nat64),
+    'buyer' : IDL.Principal,
+    'disputed_at' : IDL.Opt(IDL.Nat64),
+    'escrow_id' : IDL.Nat64,
+    'amount' : IDL.Nat64,
+    'shipped_at' : IDL.Opt(IDL.Nat64),
+    'resolved_at' : IDL.Opt(IDL.Nat64),
+    'released_at' : IDL.Opt(IDL.Nat64),
+  });
+  const Result_1 = IDL.Variant({ 'Ok' : EscrowAgreement, 'Err' : IDL.Text });
+  const Result_2 = IDL.Variant({
+    'Ok' : IDL.Tuple(IDL.Principal, IDL.Principal),
+    'Err' : IDL.Text,
+  });
+  const Result_3 = IDL.Variant({ 'Ok' : IDL.Nat64, 'Err' : IDL.Text });
+  const DisputeDecision = IDL.Variant({
+    'RefundBuyer' : IDL.Null,
+    'ReleaseFunds' : IDL.Null,
+  });
+  return IDL.Service({
+    'agree_escrow' : IDL.Func([IDL.Nat64], [Result], []),
+    'cancel_escrow' : IDL.Func([IDL.Nat64], [Result], []),
+    'confirm_goods_received' : IDL.Func(
+        [IDL.Nat64, ReleaseMethod],
+        [Result],
+        [],
+      ),
+    'confirm_goods_shipped' : IDL.Func([IDL.Nat64], [Result], []),
+    'confirm_icp_transfer' : IDL.Func([IDL.Nat64], [Result], []),
+    'fund_escrow' : IDL.Func([IDL.Nat64], [Result], []),
+    'get_escrow' : IDL.Func([IDL.Nat64], [Result_1], ['query']),
+    'get_owner' : IDL.Func([], [IDL.Opt(IDL.Principal)], ['query']),
+    'get_participants' : IDL.Func([IDL.Nat64], [Result_2], ['query']),
+    'initiate_escrow' : IDL.Func(
+        [IDL.Principal, IDL.Principal, IDL.Nat64, IDL.Text],
+        [Result_3],
+        [],
+      ),
+    'list_my_escrows' : IDL.Func([], [IDL.Vec(EscrowAgreement)], ['query']),
+    'open_dispute' : IDL.Func([IDL.Nat64], [Result], []),
+    'resolve_dispute' : IDL.Func([IDL.Nat64, DisputeDecision], [Result], []),
+    'set_admin' : IDL.Func([IDL.Principal], [Result], []),
+    'set_off_chain_server' : IDL.Func([IDL.Principal], [Result], []),
+    'set_owner' : IDL.Func([IDL.Principal], [], []),
+    'whoami' : IDL.Func([], [IDL.Principal], []),
+  });
 };
 export const init = ({ IDL }) => { return []; };
